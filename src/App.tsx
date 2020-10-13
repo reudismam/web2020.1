84fac1f66aef79957598aaac6d073a882b116280
reudismam@gmail.com <reudismam@gmail.com>
2020-10-13 16:10:24 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
+import logoImg from './images/logo.png';
 import './App.css';
+import './settings/global.css';
 
 function App() {
-  return (
+    return (
+      <div id="page">
+        <div id="container">
+          <div className="topo">
+            <div className="branding">
+              <img className="logo" src={logoImg} alt="Logo da página"/>
+              <h1>Ross Bulat</h1>
+            </div>
+
+            <div className="login">
+              <a href="">
+                Sign in
+              </a>
+              <a href="">
+                Get started
+              </a>
+            </div>  
+          </div>
+        </div>
+      </div> 
+    );
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+  {/*return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page">
+        <div className="container">
+          <div className="branding">
+            <img className="logo" src={logo} alt=""/>
+            <h3>Ross Bulant</h3>
+          </div>
+          <hr/>
+          <div className="user-container">
+            <div className="user-details">
+                <div className="user">
+                  <p>R</p>
+                </div>
+                <p>
+                Sign in to your account (<span>re__@u__.br</span>) for your personalized experience.
+                </p>
+            </div>
+            <img className="close" src={close} alt="close windows"/>
+          </div>
+          
+          <div className="login">
+            <a href="">Sign in with Google</a>
+            <p>
+
+            </p>
+          </div>
+          <hr />
+
+          <section className="main-article">
+            asff
+          </section>
+
+        </div> 
+      </div>
     </div>
-  );
+  );*/}
 }
 
 export default App;
+
+
+/*
+const Container = styled.div`
+  width: 80%;
+  background-color: blue;
+  
+  @media (min-width: 768px) {
+    background-color: tomato;
+  }
+`*/
